f9a933e378b93fd68c9c338cdfdbbf4185177a2e
Documenter.jl <documenter@juliadocs.github.io>
2026-08-25 13:57:06 +0000

    build based on 1d90f30

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"category":"section","location":"#Gtool3","page":"Home","text":"Documentation for Gtool3.\n\n","title":"Gtool3"}]
+[{"category":"section","location":"#Gtool3","page":"Home","text":"Documentation for Gtool3.\n\n","title":"Gtool3"},{"category":"method","location":"#Gtool3.headerinfo-Tuple{GTRecord}","page":"Home","text":"headerinfo(r::GTRecord)\n\nPrint a summary of the header and data dimensions of r to standard output.\n\nThe summary contains the dataset and item names, title, unit, time, date, data format, missing value, axis names and ranges, header size, and the shape of the data array. Returns nothing.\n\n\n\n\n\n","title":"Gtool3.headerinfo"}]
 }
